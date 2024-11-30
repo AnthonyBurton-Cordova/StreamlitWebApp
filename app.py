53cad532eb94deb75d8d98f48363e55068b4bb16
Anthony Burton-Cordova <anthony.burton16@yahoo.com>
2024-11-30 13:28:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+import requests
+import io
+import os
+
 st.title("Network Traffic Classification App")
 st.write("""
 This application allows you to upload network traffic data, run a pre-trained model, and receive predictions.
